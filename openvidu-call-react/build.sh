fab295868151fdded054ce37bf9ade09a5b1f31b
Luca Palermo <lucaapalermo@gmail.com>
2020-09-12 16:35:07 -0700

    Update AWS instance

--- openvidu-call-react/build.sh
+++ openvidu-call-react/build.sh
@@ -4,4 +4,4 @@ cd ../docker && \
 docker build -t watutor/openvidu-call-react:$1 --no-cache . && \
 docker push watutor/openvidu-call-react:$1 && \
 cd ../openvidu-call-react && \
-ssh -i test-openvidu.pem ubuntu@ec2-34-215-106-69.us-west-2.compute.amazonaws.com
+ssh -i ssh-openvidu-pro.pem ubuntu@ec2-44-236-41-220.us-west-2.compute.amazonaws.com
